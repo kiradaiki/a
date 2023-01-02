71de6fa26c3c019e24e17f2d7a76b59a4c82b962
rdv-neptune <121777554+rdv-neptune@users.noreply.github.com>
2023-01-02 04:37:19 +0100

    Create index.php

---
+++ hax/web/index.php
@@ -0,0 +1,120 @@
+
+
+    <script>
+
+    </script>
+
+<center><style type="text/css">
+
+*,
+*:before,
+*:after {
+  -webkit-box-sizing: inherit;
+  box-sizing: inherit;
+}
+
+html {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-size: 62.5%;
+}
+
+body {
+   background-color: #696969;
+}
+
+
+
+
+
+.sample {
+  font-size:6em;
+  text-align:center;
+  line-height:0.95em;
+  font-weight:bold;
+  color: #FFAB91;
+  text-shadow: 0.02em 0.02em 0.02em rgba(0,0,0,1);
+}
+
+.sample1 {
+  font-size:3em;
+  text-align:center;
+  line-height:0.95em;
+  font-weight:bold;
+  color: #ffffff;
+  text-shadow: 0.02em 0.02em 0.02em rgba(0,0,0,1);
+}
+
+
+.loader,
+.loader:before,
+.loader:after {
+  background: #ffffff;
+  -webkit-animation: load1 1s infinite ease-in-out;
+  animation: load1 1s infinite ease-in-out;
+  width: 1em;
+  height: 4em;
+}
+.loader {
+  color: #ffffff;
+  text-indent: -9999em;
+  margin: 88px auto;
+  position: relative;
+  font-size: 11px;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loader:before,
+.loader:after {
+  position: absolute;
+  top: 0;
+  content: '';
+}
+.loader:before {
+  left: -1.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 1.5em;
+}
+@-webkit-keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+@keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+</style></center>
+
+<script type="text/javascript">
+
+</script>
+
+<meta http-equiv="refresh" content="5;url=http://rdv-neptune.github.io/hax/mod/loader.php">
+<class="btn btn-radius-solid btn--shadow">ver 0.0.2<i class="fas fa-angle-right fa-position-right"></i></a>
+
+<a href="/mod/loader.php">execute</a></br>
+<p class="sample"><< protect installer >></p>
+<p class="sample1">Installing the application....</p>
+<div class="loader">Loading...</div>
